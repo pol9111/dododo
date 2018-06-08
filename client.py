084cc391a734ad17f05521695a3c3cd394e186ae
pol9111 <biscuit36@163.com>
2018-06-08 13:11:58 +0800

    done

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 sock_client = socket.socket() # 创建一个TCP套接字
-sock_client.connect(('127.0.0.1', 8025)) # 连接远程服务器
+sock_client.connect(('127.0.0.1', 8899)) # 连接远程服务器
 
 while True:
     msg = input('>>>') # 当收到返回时输入发送信息
